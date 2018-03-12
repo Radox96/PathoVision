9a3f8db099e09ff6d3dbee42021169ef67f6d2ec
EC2 Default User <ec2-user@ip-10-0-0-126.eu-west-1.compute.internal>
2018-03-12 14:03:17 +0000

    update

--- scripts/first_steps.py
+++ scripts/first_steps.py
@@ -1,8 +1,29 @@
 #import required packages 
-
 import sys, traceback
 import cv2
 import argparse
 import string
 import numpy as np
 import plantcv as pcv
+
+### Parse command-line arguments
+def options():
+    parser = argparse.ArgumentParser(description="Imaging processing with opencv")
+    parser.add_argument("-i", "--image", help="Input image file.", required=True)
+    parser.add_argument("-o", "--outdir", help="Output directory for image files.", required=False)
+    parser.add_argument("-r","--result", help="result file.", required= False )
+    parser.add_argument("-w","--writeimg", help="write out images.", default=False)
+    parser.add_argument("-D", "--debug", help="Turn on debug, prints intermediate images.", default=None)
+    args = parser.parse_args()
+    return args
+    
+### Main pipeline
+def main():
+    # Get options
+    args = options()
+
+    # Read image
+    img, path, filename = pcv.readimage(args.image)
+
+    # Pipeline step
+    device = 0
