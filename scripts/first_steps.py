0158d992d39975abe55fd5384dd7f4887a60b53f
EC2 Default User <ec2-user@ip-10-0-0-126.eu-west-1.compute.internal>
2018-03-13 13:39:38 +0000

    update

--- scripts/first_steps.py
+++ scripts/first_steps.py
@@ -1,10 +1,12 @@
 #import required packages 
 import sys, traceback
-import cv2
+import cv2 as cv
+import numpy as np 
+import plantcv as pcv
 import argparse
 import string
-import numpy as np
-import plantcv as pcv
+import os
+
 
 ### Parse command-line arguments
 def options():
@@ -16,4 +18,28 @@ def options():
     parser.add_argument("-D", "--debug", help="Turn on debug, prints intermediate images.", default=None)
     args = parser.parse_args()
     return args
+
+#import image 
+import os
+
+#check wether given img path is correct
+def read_img(path):
+    str -> np.ndarray
+    if os.path.isfile(path):
+        return cv2.imread(path)
+    else:
+        raise ValueError('Path provided is not a valid file: {}'.format(path))
     
+    #define img-path
+    path = '/github/PathoCV/test_img/test_001.JPG'
+    #actual img import
+    im = cv.imread(path)
+    #converte to grayscale
+    gray_img = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        
+        #write converted img 
+        cv2.imwrite('image_grey.jpg', grey_image)
+        #display converted img
+        cv2.imshow('greyscale', image_grey.jpg
+
+#End of code 
