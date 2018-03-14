a45585661527b6fc86bc0e0b9ec9f1654f386a3c
EC2 Default User <ec2-user@ip-10-0-0-126.eu-west-1.compute.internal>
2018-03-14 13:33:19 +0000

    Update_reset

--- scripts/contours.py
+++ scripts/contours.py
@@ -4,23 +4,24 @@ import numpy as np
 #define image path
 path = "/github/PathoCV/test_img/test_001.JPG"
 
-#load image
-im = cv2.imread(path, 0)
+print(path) 
 
+
+#load image
+#im = cv2.imread(path, 0)
+#cv2.imshow('image', im)
+#cv2.waitKey(0) 
+#cv2.destroyAllWindows()
 #convert to grayscale (already done with above addition of ",0")
 #imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-
 #set threshold
-ret, thresh = cv2.threshold(im, 127, 255, 0)
-
+#ret, thresh = cv2.threshold(im, 127, 255, 0)
 #generate contours
-im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
+#im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 #draw contours
-cv2.drawContours(im2, contours, -1, (0,255,0), 3)
-
+#cv2.drawContours(im2, contours, -1, (0,255,0), 3)
 #write results image
-cv2.imwrite('result.jpg',im2)
+#cv2.imwrite('result.png',im2)
 
 #End of Code
 
